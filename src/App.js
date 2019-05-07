1a186d45d103e7097005592f1fe2390a26274de0
And111rey <samu111ray@gmail.com>
2019-05-08 01:51:00 +0300

    click counter with store import

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-// import ReactDOM from "react-dom";
+import { Store } from "./store.js"
 
+
+const initialState = {count: 0};
+
+let updateState = (state, action) => {
+  switch (action.type) {
+    case "INC": return { count: state.count + action.amount };
+    case "DEC": return { count: state.count - action.amount }
+    default: return state;
+  }
+}
+
+const incrementAction = { type: "INC", amount: 1 };
+const decrementAction = { type: "DEC", amount: 1 };
+
+const store = new Store(updateState, initialState)
 
 class Counter extends Component {
   constructor (props) {
     super(props)
 
-    this.state = { count: 0 }
+    // this.state = { count: 0 }
 
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
   }
-  
-  increment() {
-    this.setState({ count: this.state.count +1  })
+
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate());
   }
-   decrement() {
-     this.setState({ count: this.state.count -1 })
-     console.log("decremrnt button  worck")
-   }
+
+  increment() {
+    store.update(incrementAction);
+  }
+  decrement() {
+    store.update(decrementAction);
+  }
    render() {
      return(
        <div className="counter">
-        <span className="count">{this.state.count}</span>
+        <span className="count">{store.state.count}</span>
 
         <div classNAme="buttons">
           <button onClick={this.increment} className="increment">+</button>
